3f9c2406c23b287506467ebdd8990a2ef2cd9899
surya pratap <146619684+suryapratap64@users.noreply.github.com>
2024-10-11 22:22:27 +0530

    Update Clockslogan.jsx

--- src/components/Clockslogan.jsx
+++ src/components/Clockslogan.jsx
@@ -1,6 +1,6 @@
 let Clockslogan=()=>{
     return (
-    <p className="sp">This is the clock that shows the time int bharat at current time</p>
+    <p className="sp">This is the clock that shows the time in the bharat at current time</p>
     );
  }
  export default Clockslogan
